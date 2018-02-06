f0fc361be5d9ecf10bdb37c1e8c43adc0fe1834f
Rick Barnhill <rbbhill@gmail.com>
2018-02-06 16:31:33 -0600

    Commit Iteration
    Project 3 completed.

--- Lesson 21/Project 3 - Pixel Art Maker/design.js
+++ Lesson 21/Project 3 - Pixel Art Maker/design.js
@@ -1,50 +1,45 @@
+// Your code goes here!
 $(function() {
-  // Select color input
   // Select size input
   // When size is submitted by the user, call makeGrid()
-
-  const submitButton = $('input[type="submit"]'),
-    canvas = $('#pixel_canvas');
-
-  submitButton.click(function(e) {
-      e.preventDefault();
+    const submitButton = $('input[type="submit"]'),
+      canvas = $('#pixel_canvas');
+//Initialize grid
+    submitButton.click(function(x) {
+      x.preventDefault();
       makeGrid();
-
-
-  function makeGrid() {
-    let col = $("#input_height").val(),
-      row = $("#input_width").val(),
-      canvasColumn,
-      canvasRow;
-
-    canvas.children().remove();
-
-    for (let i = 0; i < col; i++) {
-      canvasColumn += "<tr></tr>";
-    }
-
-    for (let j = 0; j < row; j++) {
-      canvasRow += "<td></td>";
-    }
-
-    canvas
-      .append(canvasColumn)
-      .children()
-      .append(canvasRow);
-  }
-
-
-  });
-
-  canvas.on("click", "td", function(e) {
-    e.preventDefault();
-    let color = $("#colorPicker").val(),
-      defaultColor = this.style.backgroundColor;
-    // set color and erase color to default
-    if (defaultColor === "") {
-      $(this).css("backgroundColor", color);
-    } else {
-      $(this).css("backgroundColor", "");
-    }
-  });
+//Grid height and width
+      function makeGrid() {
+        let
+          col = $("#input_height").val(),
+          canvasColumn,
+          row = $("#input_width").val(),
+          canvasRow;
+//Grid creation
+          for (let r = 0; r < col; r++) {
+            canvasColumn += "<tr></tr>";
+          }
+            for (let c = 0; c < row; c++) {
+            canvasRow += "<td></td>";
+            }
+//Clear grid
+        canvas.children().remove();
+//Add columns and rows
+        canvas.append(canvasColumn)
+        canvas.children().append(canvasRow);
+      }
+    });
+//Event Listner
+    canvas.on("click", "td", function(x) {
+      x.preventDefault();
+  // Select color input and erase
+      let color = $("#colorPicker").val(),
+        defaultColor = this.style.backgroundColor;
+      if (defaultColor === "") {
+        $(this).css("backgroundColor", color);
+        }
+      else {
+        $(this).css("backgroundColor", "");
+        }
+    });
 });
